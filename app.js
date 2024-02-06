82fd45da8ee9ca6d2653dbec9d4e0abc9b650913
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-06 12:30:10 +0600

    created conversationSchema

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-// const bcryptjs = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 
@@ -8,7 +8,7 @@ const cors = require('cors');
 require('./db/connection')
 
 // import files
-// const Users = require('./models/Users');
+const Users = require('./models/Users');
 // const Conversations = require('./models/Conversations')
 // const Messages = require('./models/Messages');
 // const { Socket } = require('socket.io');
@@ -22,8 +22,76 @@ const port = process.env.PORT || 8000;
 // socket.io
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send("Welcome")
+})
+
+app.post('/api/register', async (req, res, next) => {
+    try {
+        const { fullName, email, password } = req.body;
+
+        if (!fullName || !email || !password) {
+            res.status(404).send("Please enter the required fields")
+        } else {
+            const isAlreadyExist = await Users.findOne({ email })
+
+            if (isAlreadyExist) {
+                res.status(404).send('User already exists')
+            } else {
+                const newUser = new Users({
+                    fullName,
+                    email
+                })
+                bcryptjs.hash(password, 12, (err, hashedPassword) => {
+                    newUser.set('password', hashedPassword)
+                    newUser.save()
+                    next()
+                })
+                return res.status(200).send("User registered successfully")
+            }
+        }
+    } catch (error) {
+
+    }
+})
+
+app.post('/api/login', async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            res.status(404).send("Please enter the required fields")
+        } else {
+            const user = await Users.findOne({ email })
+
+            if (!user) {
+                res.status(400).send('User email or pass is incorrect')
+            } else {
+                const validateUser = await bcryptjs.compare(password, user.password)
+                if (!validateUser) {
+                    res.status(400).send('User email or pass is incorrect')
+                } else {
+                    const payload = {
+                        userId: user._id,
+                        email: user.email
+                    }
+                    const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || "This is a JWT secret key"
+
+                    jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: 84600 }, async (err, token) => {
+                        await Users.updateOne({ _id: user._id }, {
+                            $set: {
+                                token
+                            }
+                        })
+                        user.save()
+                        next()
+                    })
+                    res.status(200).json({ user:{email:user.email,fullName:user.fullName}, token: user.token })
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 app.listen(port, () => {
--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const uri = `mongodb+srv://gestiak08:Y5no9kAGqcmDchbe@cluster0.a37lvgd.mongodb.net/?retryWrites=true&w=majority`
 
-mongoose.connect(url, {
+mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => console.log('Connected to the db')).catch((e) => console.log('Error', e))
