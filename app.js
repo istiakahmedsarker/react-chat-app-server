b4849217ec0295017b456da9c03d38680c74e20d
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-06 21:35:03 +0600

    some format updated

--- app.js
+++ app.js
@@ -109,7 +109,15 @@ app.get('/api/conversation/:userId', async (req, res) => {
     try {
         const userId = req.params.userId;
         const conversations = await Conversations.find({ members: { $in: [userId] } });
-        res.status(200).json(conversations);
+        const conversationUserData = Promise.all(conversations.map(async (conversation) => {
+            const receiverId = conversation.members.find((member) => member !== userId);
+            const user = await Users.findById(receiverId);
+            return {
+                user: { receiverId: user._id, email: user.email, fullName: user.fullName }, conversationId: conversation._id
+
+            }
+        }))
+        res.status(200).json(await conversationUserData);
 
     } catch (error) {
         console.log(error, 'Error');
