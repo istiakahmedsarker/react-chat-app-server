98c428a0e77bccecb126ace73069750889df8fd2
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-14 12:12:47 +0600

    uri fixed

--- db/connection.js
+++ db/connection.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 
 
 const uri = `mongodb+srv://gestiak08:Y5no9kAGqcmDchbe@cluster0.a37lvgd.mongodb.net/?retryWrites=true&w=majority`
+// const uri = `mongodb+srv://property-hunter:<password>@cluster0.vfvptvp.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => // console.log('Connected to the db')).catch((e) => // console.log('Error', e))
+}).then(() =>  console.log('Connected to the db')).catch((e) =>  console.log('Error', e))
